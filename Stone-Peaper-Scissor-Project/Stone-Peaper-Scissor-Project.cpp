b2bc1d3139c24419bf70aae7041e420f088b037c
Rayan Odah <rayanodah26@gmail.com>
2026-03-22 18:32:07 +0300

    v1

--- Stone-Peaper-Scissor-Project/Stone-Peaper-Scissor-Project.cpp
+++ Stone-Peaper-Scissor-Project/Stone-Peaper-Scissor-Project.cpp
@@ -1,20 +1,170 @@
-// Stone-Peaper-Scissor-Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <iostream>  
+#include <cmath>  
 
-#include <iostream>
 
-int main()
+using namespace std;
+
+enum enChoice { Stone = 1, Paper = 2, Scissor = 3 };
+enum enwhowin { user = 1, computer = 2, Draw = 3 };
+int ReadNumbersFromUser(string Message, int from, int to)
 {
-    std::cout << "Hello World!\n";
+    int Number = 0;
+
+    do
+    {
+        cout << Message << endl;
+        cin >> Number;
+    } while (Number < from || Number > to);
+
+    return Number;
+}
+int Randomchoice(int From, int To)
+{
+
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+enChoice getchoice(int choice)
+{
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    if (choice == 1)
+        return enChoice::Stone;
+    if (choice == 2)
+        return enChoice::Paper;
+    if (choice == 3)
+        return enChoice::Scissor;
+
+}
+
+void getinputs(enChoice& userchoice, enChoice& computerchoice) {
+
+
+    userchoice = getchoice(ReadNumbersFromUser("Your Choice :[1]:Stone, [2]:Paper, [3]:Scissor ? ", 1, 3));
+    computerchoice = getchoice(Randomchoice(1, 3));
+
+}
+enwhowin computervsuser(enChoice userchoice, enChoice computerchoice) {
+
+    if (userchoice == computerchoice)
+        return enwhowin::Draw;
+    else if (userchoice == enChoice::Paper && computerchoice == enChoice::Stone)
+        return enwhowin::user;
+    else if (userchoice == enChoice::Stone && computerchoice == enChoice::Scissor)
+        return enwhowin::user;
+    else if (userchoice == enChoice::Scissor && computerchoice == enChoice::Paper)
+        return enwhowin::user;
+    else
+        return enwhowin::computer;
+}
+string whathechoice(int input) {
+    switch (input) {
+    case 1:
+        return "Stone";
+    case 2:
+        return "Paper";
+    case 3:
+        return "Scissor";
+    }
+}
+
+
+void gamedata(int gameround, int drawcounter, int Computerwincounter, int Userwincounter) {
+
+    cout << "\t________________________________________________________________" << endl;
+
+    cout << "\n\t\t\t" << "+++ " << "G a m e  O v e r " << " +++" << endl;
+    cout << "\t________________________________________________________________" << endl;
+    cout << "\n\t_________________________ [ Game Results ] _________________________" << endl;
+    cout << "\n\tGame Rounds       : " << gameround << endl;
+    cout << "\tPlayer won times  : " << Userwincounter << endl;
+    cout << "\tComputer won times: " << Computerwincounter << endl;
+    cout << "\tDraw times        : " << drawcounter << endl;
+
+    if (Userwincounter > Computerwincounter) {
+        cout << "\tFinal Winner  : " << "Player" << endl;
+        system("color 2F");
+    }
+    else if (Userwincounter < Computerwincounter) {
+        cout << "\tFinal Winner  : " << "Computer" << endl;
+        system("color 4F");
+        cout << "\a";
+    }
+    else
+        system("color 6F");
+
+    cout << "\t_________________________________________________________________" << endl;
+
+}
+
+
+void mainop() {
+
+    int manygamerounds = ReadNumbersFromUser("How Many Rounds 1 to 10 ", 1, 10);
+    enChoice userchoice;
+    enChoice computerchoice;
+    int drawcounter = 0, Computerwincounter = 0, Userwincounter = 0;
+
+    for (int counter = 1; counter <= manygamerounds; counter++) {
+
+        cout << "\nRound" << " [" << counter << "] " << "begins:" << endl;
+        getinputs(userchoice, computerchoice);
+        cout << "_______________" << "Round" << " [" << counter << "] " << "____________________" << endl;
+        cout << "\nPlayer 1 Choice: " << whathechoice(userchoice) << endl;
+        cout << "Computer Choice: " << whathechoice(computerchoice) << endl;
+
+        switch (computervsuser(userchoice, computerchoice)) {
+        case enwhowin::Draw:
+            cout << "Round Winner     :" << "[" << "No Winner" << "]" << endl;
+            drawcounter++;
+            system("color 6F");
+            break;
+
+        case enwhowin::user:
+            cout << "Round Winner     :" << "[" << "Player win" << "]" << endl;
+            Userwincounter++;
+
+            system("color 2F");
+            break;
+        case (enwhowin::computer):
+
+            cout << "Round Winner     :" << "[" << "computer" << "]" << endl;
+            Computerwincounter++;
+            system("color 4F");
+            cout << "\a";
+
+            break;
+
+        }
+
+        cout << "_________________________________" << endl;
+
+
+    }
+
+    gamedata(manygamerounds, drawcounter, Computerwincounter, Userwincounter);
+
+
+
+
+}
+
+
+int main() {
+    srand((unsigned)time(NULL));
+
+
+    char PlayorNo = 'Y';
+    do {
+
+        system("cls");
+        system("color 0F");
+
+        mainop();
+        cout << "\n\t\t\tdo you want play again Y/N ? ";
+        cin >> PlayorNo;
+
+    } while (PlayorNo == 'Y' || PlayorNo == 'y');
+
+
+}
